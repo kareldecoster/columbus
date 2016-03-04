d51c22dc445515605852f86523f4131dd6c20dc7
Karel De Coster <kareldecoster@users.noreply.github.com>
2016-03-04 16:48:09 +0100

    Create roomba_control.h

---
+++ include/roomba_control.h
@@ -0,0 +1,48 @@
+#ifndef ROOMBA_CONTROL_H
+#define ROOMBA_CONTROL_H
+
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+#define BAUD_RATE B115200	//Default Baud rate for Roomba 620 is 115200
+#define SERIAL_INTERFACE "/dev/ttyO1"	//This is the BB-UART1 on the Beaglebone Black
+
+typedef struct roomba roomba_t;
+
+roomba_t* roomba_create();
+
+int roomba_destroy(roomba_t** roomba);
+
+int roomba_start(roomba_t* roomba);
+
+int roomba_control(roomba_t* roomba);
+
+int roomba_safe_mode(roomba_t* roomba);
+
+int roomba_full_mode(roomba_t* roomba);
+
+int roomba_forward(roomba_t* roomba);
+
+int roomba_reverse(roomba_t* roomba);
+
+int roomba_clean(roomba_t* roomba);
+
+int roomba_save_song_jingle_bells(roomba_t* roomba);
+
+int roomba_save_song_imperial(roomba_t* roomba);
+
+int roomba_play_song(roomba_t* roomba);
+
+int roomba_turn_cw(roomba_t* roomba);
+
+int roomba_turn_ccw(roomba_t* roomba);
+
+int roomba_stop(roomba_t* roomba);
+
+int roomba_drive_distance(roomba_t* roomba);
+
+int roomba_play_script(roomba_t* roomba);
+#endif
