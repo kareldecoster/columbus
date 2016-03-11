7e5b4950a616f540eb0efdc903256ce6bce6a772
Karel De Coster <kareldecoster@users.noreply.github.com>
2016-03-11 12:11:06 +0100

    Update roomba_control.h

--- include/roomba_control.h
+++ include/roomba_control.h
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <math.h>
 
 #define BAUD_RATE B115200	//Default Baud rate for Roomba 620 is 115200
 #define SERIAL_INTERFACE "/dev/ttyO1"	//This is the BB-UART1 on the Beaglebone Black
@@ -24,9 +25,9 @@ int roomba_safe_mode(roomba_t* roomba);
 
 int roomba_full_mode(roomba_t* roomba);
 
-int roomba_forward(roomba_t* roomba);
+int roomba_forward(roomba_t* roomba, int16_t speed);
 
-int roomba_reverse(roomba_t* roomba);
+int roomba_reverse(roomba_t* roomba, int16_t speed);
 
 int roomba_clean(roomba_t* roomba);
 
@@ -34,7 +35,7 @@ int roomba_save_song_jingle_bells(roomba_t* roomba);
 
 int roomba_save_song_imperial(roomba_t* roomba);
 
-int roomba_play_song(roomba_t* roomba);
+int roomba_play_song(roomba_t* roomba, char number);
 
 int roomba_turn_cw(roomba_t* roomba);
 
@@ -42,7 +43,12 @@ int roomba_turn_ccw(roomba_t* roomba);
 
 int roomba_stop(roomba_t* roomba);
 
-int roomba_drive_distance(roomba_t* roomba);
+int roomba_drive_distance(roomba_t* roomba, int mm);
 
 int roomba_play_script(roomba_t* roomba);
+
+double roomba_request_angle(roomba_t* roomba);
+
+int16_t roomba_request_distance(roomba_t* roomba);
+
 #endif
