4f18f895870e3a68017e89f108243e4139698a1c
Karel De Coster <kareldecoster@users.noreply.github.com>
2016-03-14 17:35:11 +0100

    reading in sensor values
    added the possibility to read in sensors and to allow turning a certain angle

--- include/roomba_control.h
+++ include/roomba_control.h
@@ -6,12 +6,28 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <stdint.h>
 #include <math.h>
 
 #define BAUD_RATE B115200	//Default Baud rate for Roomba 620 is 115200
 #define SERIAL_INTERFACE "/dev/ttyO1"	//This is the BB-UART1 on the Beaglebone Black
+#define ROOMBA_WIDTH	234	//in mm
 
 typedef struct roomba roomba_t;
+
+typedef enum {
+	B_LEFT,
+	B_RIGHT,
+	B_BOTH,
+	B_NONE
+}bumpstate_t;
+
+typedef enum{
+	C_NO_CLIFF,
+	C_RIGHT,
+	C_LEFT,
+	C_FRONT
+}cliffstate_t;
 
 roomba_t* roomba_create();
 
@@ -50,5 +66,19 @@ int roomba_play_script(roomba_t* roomba);
 double roomba_request_angle(roomba_t* roomba);
 
 int16_t roomba_request_distance(roomba_t* roomba);
+
+double roomba_turn_degree_cw(roomba_t* roomba, int16_t speed, double angle);
+
+double roomba_turn_degree_ccw(roomba_t* roomba, int16_t speed, double angle);
+
+double roomba_get_battery(roomba_t* roomba);
+
+int roomba_get_wall(roomba_t* roomba);
+
+bumpstate_t roomba_get_bumpstate(roomba_t* roomba);
+
+cliffstate_t roomba_get_cliffstate(roomba_t* roomba);
+
+int roomba_cover_and_dock(roomba_t* roomba);
 
 #endif
